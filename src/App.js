3149525be18d4bf80a1add11eb9c5903dc6fdd68
chillag09733 <chillag09733@gmail.com>
2024-11-23 09:14:19 +0100

    alapszerk

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <article></article>
+      <footer>@massanyicsilla</footer>
+    </div>
+  );
+}
+
+export default App;
